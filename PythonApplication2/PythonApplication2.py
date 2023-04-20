59464e2c46588d5f1689e67131991e107109dd7d
Your Name <you@example.com>
2023-04-20 23:46:00 +0300

    First commit

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,13 +1,13 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Description for program')
 
-parser.add_argument('integers', metavar='Nigger', type=int, nargs=3,
-                    help='an integer for the accumulator')
+parser.add_argument('positional_argument', metavar='posarg', type=int, nargs=2,
+                    help='explain message for parameter')
 
-parser.add_argument('--sum', dest='accumulate', action='store_const',
+parser.add_argument('-s', '--sum', dest='sum', action='store_const',
                     const=sum, default=max,
                     help='sum the integers (default: find the max)')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+print(args.sum(args.positional_argument))
